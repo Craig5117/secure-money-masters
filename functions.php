aa5020e113c5e9410947aa4219d30a5a829cb0ad
Craig5117 <craig.bennett5117@gmail.com>
2021-06-22 17:18:39 -0400

    set up dynamic header menu, still needs styling

--- functions.php
+++ functions.php
@@ -3,8 +3,10 @@
         wp_enqueue_style('main-styles', get_stylesheet_uri());
     }
     add_action('wp_enqueue_scripts', 'smm_files');
+
     function smm_features() {
         add_theme_support('title-tag');
+        register_nav_menu('headerMenuLocation', 'Header Menu Location');
     }
     add_action('after_setup_theme', 'smm_features');
 ?>
--- header.php
+++ header.php
@@ -7,13 +7,20 @@
     </head>
     <body <?php body_class()?>>
         <header>
-            <div>
+            <div class="header-container">
                 <h1>
                     <a href="<?php echo site_url() ?>" style="text-decoration: none; color: #165d00">
                         Secure Money Masters
                     </a>
                 </h1>
                 <nav>
+                    <div class="nav-options-container">
+                        <?php wp_nav_menu(array(
+                            'theme_location' => 'headerMenuLocation',
+                            'menu_class' => 'header-menu'
+                            )); 
+                        ?>
+                    </div>
                 </nav>              
             </div>
         </header>
