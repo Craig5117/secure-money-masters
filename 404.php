9760af77aeda035b301ddce67c04f5243110401e
Craig5117 <craig.bennett5117@gmail.com>
2021-07-19 16:07:32 -0400

    added 404 page and styled

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php 
+
+get_header(); ?>
+
+<div class="page-title-container">
+    <h2 class="page-title">404 Not Found</h2>
+</div>  
+
+
+<main class="content">
+		<div class="error-message-container">
+            <h3 class="error-heading">404</h3>
+            <div class="error-text">
+                <p>We're sorry. It appears the location you tried to visit does not exist.</p>
+                <p>Please try clicking one of the links in the menu to navigate to a diffrent location on our site.</p>
+            </div>
+		</div><!-- .page-content -->
+</main>
+<?php
+
+get_footer(); ?>
