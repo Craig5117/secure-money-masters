58f9a01e944acf345869c3abdefe985c07da934d
Craig5117 <craig.bennett5117@gmail.com>
2021-10-13 09:47:09 -0400

    made some edits to the theme for production

--- front-page.php
+++ front-page.php
@@ -1,9 +1,10 @@
 <?php get_header(); 
     while ( have_posts() ) {
         the_post(); ?>
-    <div class="hero">
-        <img alt="Secure Money Masters Logo" class="hero-image" src="<?php echo get_theme_file_uri('/images/smm-banner-green-subtitle-reworked-light.webp') ?>"/>
+    <div class="hero">    
+        <img alt="Secure Money Masters Logo" class="hero-image" src="<?php echo get_theme_file_uri('/images/smm-banner-dark-green-gold-background-2.webp') ?>"/>
     </div>
+    <p class="broker-check-bubble">Check the background of your financial professional on FINRA's <a style="color: black" href="https://brokercheck.finra.org/">BrokerCheck</a><p>
     <main class="content">
         <?php
             the_content(); ?>
--- page.php
+++ page.php
@@ -6,9 +6,8 @@ while ( have_posts() ) {
     the_post(); ?>
     <div class="page-title-container">
       <h2 class="page-title"><?php the_title() ?></h2>
-    </div>  
-  
-    
+    </div>
+    <p class="broker-check-bubble">Check the background of your financial professional on FINRA's <a style="color: black" href="https://brokercheck.finra.org/">BrokerCheck</a><p> 
     <main class="content">
         <?php the_content() ?>
 </main>
